a21c57cdb852f7e2b5385deeec97f62319ebd4c1
flag522 <m18791029179@163.com>
2022-12-16 16:56:14 +0800

    字体

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,8 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+import "@chinese-fonts/dyh/dist/SmileySans-Oblique/result.css";
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: '烽淋氨埂',
